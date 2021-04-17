052cc872aada986bd227be4b3d793c05e99402c2
Matthew Blanchard <matthewblanc@msn.com>
2021-04-16 20:44:20 -0400

    Added delete selected points with backspace.

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,6 +1,6 @@
 //! Global thread local state.
 
-use glifparser::{Contour, Glif, WhichHandle, MFEKGlif, Point, glif::{HistoryEntry, HistoryType, Layer}};
+use glifparser::{Contour, Glif, MFEKGlif, Point, PointType, WhichHandle, glif::{HistoryEntry, HistoryType, Layer}};
 pub use crate::state::Follow;
 
 pub use crate::renderer::console::Console as RendererConsole;
@@ -137,10 +137,65 @@ impl Editor {
         self.modifying = true;
     }
 
-    // TODO: split following 3 functions off into some tool utility file
+    pub fn is_point_selected(&self, contour_idx: usize, point_idx: usize) -> bool
+    {
+        if let Some(editor_pidx) = self.point_idx {
+            let editor_cidx = self.contour_idx.unwrap();
 
-    pub fn delete_selection(&self) {
+            if contour_idx == editor_cidx && point_idx == editor_pidx { return true };
+        }
+
+        if self.selected.contains(&(contour_idx, point_idx)) { return true };
+
+        return false;
+    }
+
+    // TODO: split following 3 functions off into some tool utility file
+    pub fn delete_selection(&mut self) {
+        self.begin_layer_modification("Delete selection.");
         
+        let layer = &self.glyph.as_ref().unwrap().glif.layers[self.layer_idx.unwrap()];
+        let mut new_outline: Vec<Vec<Point<PointData>>> = Vec::new();
+        for (contour_idx, contour) in layer.outline.as_ref().unwrap().iter().enumerate() {
+            let mut results = Vec::new();
+            let mut cur_contour = Vec::new();
+            let mut deleted = false;
+            for (point_idx, point) in contour.iter().enumerate() {
+                let to_delete = self.is_point_selected(contour_idx, point_idx);
+
+                if to_delete {
+                    results.push(cur_contour);
+                    cur_contour = Vec::new();
+                    deleted = true;
+                } else  {
+                    cur_contour.push(point.clone());
+                }
+            }
+            results.push(cur_contour);
+
+            if results.len() > 1 && contour.first().unwrap().ptype != PointType::Move {
+                let mut move_to_front = results.pop().unwrap().clone();
+                move_to_front.append(&mut results[0]);
+                results[0] = move_to_front;
+            }
+
+            for mut result in results {
+                if result.len() != 0 {
+                    if deleted {
+                        result.first_mut().unwrap().ptype = PointType::Move;
+                    }
+                    new_outline.push(result); 
+                }
+            }
+        }
+
+        self.glyph.as_mut().unwrap().glif.layers[self.layer_idx.unwrap()].outline = Some(new_outline);
+
+        self.end_layer_modification();
+
+        self.contour_idx = None;
+        self.point_idx = None;
+        self.selected = HashSet::new();
     }
     /// Checks if the active point is the active contour's start or end. Does not modify.
     pub fn get_contour_start_or_end(&self, contour_idx: usize, point_idx: usize) -> Option<SelectPointInfo>
