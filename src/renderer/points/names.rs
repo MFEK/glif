73c8ea976bb4c47ba9adb3c9b87a15a852beaf67
Fredrick Brennan <copypaste@kittens.ph>
2021-05-05 09:28:33 -0400

    Implement UiString instead of old fn's in renderer::string

--- src/renderer/points/names.rs
+++ src/renderer/points/names.rs
@@ -1,14 +1,16 @@
 use skulpin::skia_safe::Canvas;
 
 use crate::editor::Editor;
-use crate::renderer::string::draw_string_at_point;
+use crate::renderer::string::UiString;
 
 pub fn draw_point_number(v: &Editor, at: (f32, f32), number: isize, canvas: &mut Canvas) {
     let converted = number.to_string();
-    draw_string_at_point(v, at, &converted, canvas);
+    let uis = UiString::new(&converted);
+    uis.draw(v, at, canvas);
 }
 
 pub fn draw_point_location(v: &Editor, at: (f32, f32), original: (f32, f32), canvas: &mut Canvas) {
     let converted = format!("{}, {}", original.0, original.1);
-    draw_string_at_point(v, at, &converted, canvas);
+    let uis = UiString::new(&converted);
+    uis.draw(v, at, canvas);
 }
