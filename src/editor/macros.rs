7296a1f5385e58e12e8a22b5c406fb37ed11593a
Fredrick Brennan <copypaste@kittens.ph>
2021-05-13 15:15:32 -0400

    Stop Point Info from force closing contours
    Among other bugs fixed:
    
    * Do not display A/B where uncalled for;
    * Make previous point a Curve if necessary;
    * Fix Bθ so that it moves the correct handle!

--- src/editor/macros.rs
+++ src/editor/macros.rs
@@ -33,3 +33,8 @@ macro_rules! get_point {
         $v.outline[$cidx].inner[$pidx]
     };
 }
+
+// This re-import is here because I think it's messy to refer to these macros using the top-level
+// crate::. This allows me to have in modules e.g. `use crate::editor::macros::get_point`, which is
+// our preferred way of importing them.
+pub use {get_contour, get_contour_mut, get_contour_len, get_contour_type, get_point};
--- src/tools/select/dialog.rs
+++ src/tools/select/dialog.rs
@@ -1,6 +1,7 @@
 use super::Select;
 
 use crate::editor::Editor;
+use crate::editor::macros::{get_contour_len, get_contour_type, get_point};
 use crate::user_interface;
 use crate::renderer::constants::PI;
 
@@ -31,7 +32,7 @@ fn imgui_decimal_text_field(label: &str, ui: &imgui::Ui, data: &mut f32) {
 }
 
 
-fn imgui_radius_theta<PD: PointData>(label: &str, ui: &imgui::Ui, ar: f32, atheta: f32, point: &mut Point<PD>) {
+fn imgui_radius_theta<PD: PointData>(label: &str, ui: &imgui::Ui, ar: f32, atheta: f32, wh: WhichHandle, point: &mut Point<PD>, ) {
     let r_label = imgui::im_str!("{}r", label);
     let theta_label = imgui::im_str!("{}θ", label);
     // Ar
@@ -66,7 +67,7 @@ fn imgui_radius_theta<PD: PointData>(label: &str, ui: &imgui::Ui, ar: f32, athet
             new_theta = athetas.to_str().parse().unwrap();
         }
         if new_r != f32::NAN && new_theta != f32::NAN {
-            point.set_polar(WhichHandle::A, (new_r, new_theta));
+            point.set_polar(wh, (new_r, new_theta));
         }
     }
 }
@@ -89,9 +90,16 @@ impl Select {
         let (tx, ty, tw, th) = user_interface::get_tools_dialog_rect(v);
         let mut orig_point: Point<_> = Point::new();
         let mut point: Point<MFEKPointData> = Point::new();
+        let mut should_make_next_point_curve: bool = false;
+        let on_open_contour = v.with_active_layer(|l| get_contour_type!(l, ci) == PointType::Move);
+        let contour_len = v.with_active_layer(|l| get_contour_len!(l, ci));
         v.with_active_layer(|layer| {
-            point = layer.outline[ci].inner[pi].clone();
+
+            point = get_point!(layer, ci, pi).clone();
             orig_point = point.clone();
+
+            let on_last_open_point: bool = pi == contour_len-1 && on_open_contour;
+            let on_first_open_point: bool = pi == 0 && on_open_contour;
 
             imgui::Window::new(
                     &if multiple_points_selected {
@@ -125,59 +133,75 @@ impl Select {
                     imgui_decimal_text_field("X", ui, &mut point.x);
                     // Y
                     imgui_decimal_text_field("Y", ui, &mut point.y);
-                    // A
-                    ui.text(imgui::im_str!("Previous off-curve point"));
-                    let mut a_colocated = point.a == Handle::Colocated;
-                    ui.checkbox(imgui::im_str!("A Colocated"), &mut a_colocated);
-                    // AX
-                    let (mut ax, mut ay) = point.handle_or_colocated(WhichHandle::A, |f|f, |f|f);
-                    let orig_axy = (ax, ay);
-                    imgui_decimal_text_field("AX", ui, &mut ax);
-                    // AY
-                    imgui_decimal_text_field("AY", ui, &mut ay);
 
-                    if (ax, ay) != orig_axy {
-                        point.a = Handle::At(ax, ay);
-                        point.ptype = PointType::Curve;
-                    } else if a_colocated {
-                        point.a = Handle::Colocated;
+                    // A
+                    let mut a_colocated = point.a == Handle::Colocated;
+                    if !on_last_open_point {
+                        ui.text(imgui::im_str!("Next off-curve point"));
+                        ui.checkbox(imgui::im_str!("A Colocated"), &mut a_colocated);
+                        // AX
+                        let (mut ax, mut ay) = point.handle_or_colocated(WhichHandle::A, |f|f, |f|f);
+                        let orig_axy = (ax, ay);
+                        imgui_decimal_text_field("AX", ui, &mut ax);
+                        // AY
+                        imgui_decimal_text_field("AY", ui, &mut ay);
+
+                        if (ax, ay) != orig_axy {
+                            point.a = Handle::At(ax, ay);
+                            point.ptype = PointType::Curve;
+                            should_make_next_point_curve = true;
+                        } else if a_colocated {
+                            point.a = Handle::Colocated;
+                        }
+                        // ArΘ
+                        let (ar, mut atheta) = point.polar(WhichHandle::A);
+                        atheta = atheta * (180. / PI);
+                        atheta -= 180.;
+                        imgui_radius_theta("A", ui, ar, atheta, WhichHandle::A, &mut point);
                     }
-                    // ArΘ
-                    let (ar, mut atheta) = point.polar(WhichHandle::A);
-                    atheta = atheta * (180. / PI);
-                    atheta -= 180.;
-                    imgui_radius_theta("A", ui, ar, atheta, &mut point);
 
                     // B
-                    ui.text(imgui::im_str!("Next off-curve point"));
                     let mut b_colocated = point.b == Handle::Colocated;
-                    ui.checkbox(imgui::im_str!("B Colocated"), &mut b_colocated);
-                    // BX
-                    let (mut bx, mut by) = point.handle_or_colocated(WhichHandle::B, |f|f, |f|f);
-                    let orig_bxy = (bx, by);
-                    imgui_decimal_text_field("BX", ui, &mut bx);
-                    // BY
-                    imgui_decimal_text_field("BY", ui, &mut by);
-                    if (bx, by) != orig_bxy {
-                        point.b = Handle::At(bx, by);
-                        point.ptype = PointType::Curve;
-                    } else if b_colocated {
-                        if a_colocated {
-                            point.ptype = PointType::Line;
+                    if !on_first_open_point {
+                        ui.text(imgui::im_str!("Previous off-curve point"));
+                        ui.checkbox(imgui::im_str!("B Colocated"), &mut b_colocated);
+                        // BX
+                        let (mut bx, mut by) = point.handle_or_colocated(WhichHandle::B, |f|f, |f|f);
+                        let orig_bxy = (bx, by);
+                        imgui_decimal_text_field("BX", ui, &mut bx);
+                        // BY
+                        imgui_decimal_text_field("BY", ui, &mut by);
+                        if (bx, by) != orig_bxy {
+                            point.b = Handle::At(bx, by);
+                            point.ptype = PointType::Curve;
+                        } else if b_colocated {
+                            point.b = Handle::Colocated;
                         }
-                        point.b = Handle::Colocated;
+                        // BrΘ
+                        let (br, mut btheta) = point.polar(WhichHandle::B);
+                        btheta = btheta * (180. / PI);
+                        btheta -= 180.;
+                        if btheta.is_sign_positive() {
+                            btheta = 360. - btheta;
+                        }
+                        imgui_radius_theta("B", ui, br, btheta, WhichHandle::B, &mut point);
                     }
-                    // BrΘ
-                    let (br, mut btheta) = point.polar(WhichHandle::B);
-                    btheta = btheta * (180. / PI);
-                    imgui_radius_theta("B", ui, br, btheta, &mut point);
                 });
         });
 
         if orig_point.x != point.x || orig_point.y != point.y || orig_point.a != point.a || orig_point.b != point.b || orig_point.ptype != point.ptype {
             v.begin_layer_modification("Point properties changed (dialog)");
             v.with_active_layer_mut(|layer| {
-                layer.outline[ci].inner[pi] = point.clone();
+                if get_point!(layer, ci, pi).ptype == PointType::Move {
+                    point.ptype = PointType::Move;
+                }
+                if should_make_next_point_curve {
+                    let ppi = if pi == contour_len-1 { 0 } else { pi + 1 };
+                    if !(on_open_contour && ppi == 0) {
+                        get_point!(layer, ci, ppi).ptype = PointType::Curve;
+                    }
+                }
+                get_point!(layer, ci, pi) = point.clone();
             });
             v.end_layer_modification();
         }
