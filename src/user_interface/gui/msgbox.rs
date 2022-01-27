fd9fd45805e8d06b60b007228d327cc128d540b9
Fredrick Brennan <copypaste@kittens.ph>
2022-01-27 01:58:11 -0500

    One can't initialize GTK from multiple threads
    So we could not spawn multiple gui::error!'s.
    
    …Maybe in future research if one can "deinit" GTK?

--- src/user_interface/gui/msgbox.rs
+++ src/user_interface/gui/msgbox.rs
@@ -1,15 +1,15 @@
 macro_rules! gui_error {
-    ($($format_args:tt),*) => {
-        use log;
-        use msgbox::{self, IconType::*};
-        use std::thread;
-        log::error!($($format_args),*);
-        thread::spawn(|| {
-            match msgbox::create("MFEKglif critical error", &format!($($format_args),*), Error) {
+    ($($format_args:expr),*) => {
+        {
+            use log;
+            log::error!($($format_args),*);
+            let err = format!($($format_args),*);
+            use msgbox::{self, IconType::Error};
+            match msgbox::create("MFEKglif critical error", &err, Error) {
                 Ok(_) => log::trace!("Opened crash msgbox successfully"),
-                Err(e) => log::error!("Failed to create error box! {:?}", e),
+                Err(msgbox_e) => log::error!("Failed to create error box! {:?}", msgbox_e),
             }
-        });
+        }
     }
 }
 pub(crate) use gui_error;
